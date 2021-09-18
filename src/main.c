bececbb883c83dbf3327775594988caf55c83cd4
Leandro Santos <leandro.santos@idea-ip.com>
2021-09-18 11:13:44 -0300

    Timer implementation on ESP32

--- src/main.c
+++ src/main.c
@@ -1,28 +1,52 @@
+/**
+ * ESP32-Exercicio-Timer - 
+ * 
+ */
 #include <stdio.h>
 #include "sdkconfig.h"
-
-// FreeRTOS functions 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-
-// Espressif functions 
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 #include "esp_log.h"
-
-//Adding GPIOs configuration 
 #include "driver/gpio.h"
 
+#include "driver/timer.h"
+
 //Global defines
-#define BLINK_GPIO GPIO_NUM_13
-#define TASK_BLINK_GPIO GPIO_NUM_12
+#define ESP32_BLINK_GPIO GPIO_NUM_13
+#define GREEN_LED_GPIO GPIO_NUM_19
+#define RED_LED_GPIO GPIO_NUM_21
+
 #define TACTILE_GPIO GPIO_NUM_22
+
+
+#define TIMER_DIVIDER (16)
+#define TIMER_SCALE (TIMER_BASE_CLK / TIMER_DIVIDER)
 
 //Variables defines
 uint32_t counter = 0;
 uint32_t led_flag = 0;
+bool led_ctrl = 0;
 
-// callback do botão 
+// TIMER Structure for our code
+typedef struct{
+    int timer_group;
+    int timer_idx;
+    int alarm_interval;
+    bool auto_reload;
+} esp_timer_info_t;
+
+// Timer event structure 
+typedef struct{
+    esp_timer_info_t info;
+    uint64_t timer_counter_value;
+}esp_timer_event_t;
+
+
+/**
+ * Tactile button callback function
+ */
 static void IRAM_ATTR gpio_isr_handler(void *arg){
     if (TACTILE_GPIO == (uint32_t) arg){
         if (gpio_get_level(TACTILE_GPIO) == 0){
@@ -30,7 +54,8 @@ static void IRAM_ATTR gpio_isr_handler(void *arg){
         }
         led_flag = 1;
     }
-};
+}
+
 
 
 void blink_task(void *pvParameter)
@@ -43,15 +68,17 @@ void blink_task(void *pvParameter)
     */
     while(1) {
         /* Blink off (output low) */
-        gpio_set_level(TASK_BLINK_GPIO, 0);
-        vTaskDelay(1050 / portTICK_PERIOD_MS);
+        gpio_set_level(ESP32_BLINK_GPIO, 0);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
         /* Blink on (output high) */
-        gpio_set_level(TASK_BLINK_GPIO, 1);
-        vTaskDelay(1050 / portTICK_PERIOD_MS);
+        gpio_set_level(ESP32_BLINK_GPIO, 1);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
     }
 };
 
-// Identify ESP32 Device type and memory
+/**
+ * Identify ESP32 Device type and memory function
+ */
 void ESP32_IDENTIFY_DEVICE_TYPE(void)
 {
     printf("***** ESP32 INICIALIZATION (by Leandro Santos) ***** \n");
@@ -73,7 +100,9 @@ void ESP32_IDENTIFY_DEVICE_TYPE(void)
 };
 
 
-// Configure all GPIOs 
+/**
+ * GPIOs configuration and initialize 
+ */
 void USER_GPIO_INIT(void)
 {
     //insert atributes for tactile button 
@@ -91,7 +120,7 @@ void USER_GPIO_INIT(void)
     gpio_config_t led_user_config = {
         .intr_type = GPIO_INTR_DISABLE,
         .mode = GPIO_MODE_OUTPUT,
-        .pin_bit_mask = (1ULL << BLINK_GPIO) | (1ULL << TASK_BLINK_GPIO),
+        .pin_bit_mask = (1ULL << ESP32_BLINK_GPIO) | (1ULL << GREEN_LED_GPIO) | (1ULL << RED_LED_GPIO),
         .pull_down_en = 0
     };
     //initilize tactile button 
@@ -103,13 +132,72 @@ void USER_GPIO_INIT(void)
 };
 
 
-/* 
-    Main Application execution 
+/**
+ * TIMER callback function
+ */
+static bool IRAM_ATTR timer_group_isr_callback(void *args){
+    BaseType_t high_task_awoken = pdFALSE;
+    esp_timer_info_t *info = (esp_timer_event_t *) args;
+    
+    uint64_t timer_counter_value = timer_group_get_counter_value_in_isr(info->timer_group, info->timer_idx);
+
+    esp_timer_event_t evt = {
+        .info.timer_group = info->timer_group,
+        .info.timer_idx = info->timer_idx,
+        .info.auto_reload = info->auto_reload,
+        .info.alarm_interval = info->alarm_interval,
+        .timer_counter_value = timer_counter_value
+    };
+    
+    if (!info->auto_reload){
+        timer_counter_value += info->alarm_interval * TIMER_SCALE;
+        timer_group_set_alarm_value_in_isr (info->timer_group, info->timer_idx, timer_counter_value);
+    }
+
+    gpio_set_level(RED_LED_GPIO, led_ctrl);
+    led_ctrl = !led_ctrl;
+
+    return high_task_awoken == pdTRUE;
+}
+/**
+ * Timer initialize
+ */
+void USER_TIMER_INIT(void){
+    timer_config_t config = {
+        .divider = TIMER_DIVIDER,        
+        .counter_dir = TIMER_COUNT_UP,   
+        .counter_en = TIMER_PAUSE,       
+        .alarm_en = TIMER_ALARM_EN,      
+        .auto_reload = true,             
+    };
+    timer_init(TIMER_GROUP_0, TIMER_0, &config);
+
+    uint32_t intervalo_em_segundos = 5;
+
+    timer_set_counter_value(TIMER_GROUP_0, TIMER_0, 0); 
+    timer_set_alarm_value(TIMER_GROUP_0, TIMER_0, intervalo_em_segundos * TIMER_SCALE);
+    timer_enable_intr(TIMER_GROUP_0, TIMER_0); 
+
+    esp_timer_info_t *timer_info = calloc(1, sizeof(esp_timer_info_t));
+    timer_info->timer_group = TIMER_GROUP_0;
+    timer_info->timer_idx = TIMER_0;
+    timer_info->auto_reload = true;
+    timer_info->alarm_interval = intervalo_em_segundos; 
+    timer_isr_callback_add(TIMER_GROUP_0, TIMER_0, timer_group_isr_callback, timer_info, 0);
+
+    timer_start(TIMER_GROUP_0, TIMER_0);
+}
+
+
+/**
+ *  Main Application execution 
 */
 void app_main(void){
     ESP32_IDENTIFY_DEVICE_TYPE();
 
     USER_GPIO_INIT();
+
+    USER_TIMER_INIT();
 
     xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
 
